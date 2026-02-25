231620c93241e204b7c314bae4d496e56322e474
illFoXlli <fox.ruden@egmail.com>
2026-02-25 20:34:26 +0200

    Add MyQueue implementation with tests

--- src/test/java/com/fox/homework09/collections/MyQueueTest.java
+++ src/test/java/com/fox/homework09/collections/MyQueueTest.java
@@ -1,4 +1,50 @@
 package com.fox.homework09.collections;
 
-public class MyQueueTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyQueueTest {
+
+    @Test
+    void testAddAndPeek() {
+        MyQueue queue = new MyQueue();
+        queue.add("A");
+        queue.add("B");
+
+        assertEquals("A", queue.peek());
+        assertEquals(2, queue.size());
+    }
+
+    @Test
+    void testPoll() {
+        MyQueue queue = new MyQueue();
+        queue.add("A");
+        queue.add("B");
+
+        assertEquals("A", queue.poll());
+        assertEquals("B", queue.peek());
+        assertEquals(1, queue.size());
+    }
+
+    @Test
+    void testPollFromEmpty() {
+        MyQueue queue = new MyQueue();
+
+        assertNull(queue.poll());
+        assertNull(queue.peek());
+        assertEquals(0, queue.size());
+    }
+
+    @Test
+    void testClear() {
+        MyQueue queue = new MyQueue();
+        queue.add(1);
+        queue.add(2);
+
+        queue.clear();
+
+        assertEquals(0, queue.size());
+        assertNull(queue.peek());
+    }
 }
